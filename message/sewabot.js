eb97448248e39ec5953802c84a2513a6dd297c29
abdibot <abdialamanah75@gmail.com>
2022-03-22 07:54:42 +0800

    VANZZOFC

---
+++ message/sewabot.js
@@ -0,0 +1,56 @@
+
+exports.dada = (prefix, settings, pushname, ucapanWaktu) => {
+return `${ucapanWaktu} kak ${pushname}
+Berikut adalah list harga untuk sewa botz
+
+*╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
+*┴*
+*│▸* 1 hari = Rp.1.000
+*│▸* 1 Bulan = Rp.3.000   
+*│▸* 1 Premium = Rp.5.000      
+*│▸* 1 Permanen = Rp.10.000
+*┬*
+*╰────────────[*
+
+*╭─▸*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
+*┴*
+*│▸* Shopee : ${settings.setPayment.Shopee}
+*│▸* Gopay : ${settings.setPayment.Gopay}
+*│▸* Dana : ${settings.setPayment.Dana}
+*│▸* Ovo : ${settings.setPayment.Ovo}
+*┬*
+*╰────────────[*
+
+𝗡𝗢𝗧𝗘 : 
+
+Bot on 24 jam tapi kadang 
+juga mati klo lgi ada error 
+atau lgi perbaikan bug.
+
+Kalo mau sewa bisa chat
+owner Bot, khusus untuk free 
+sewa 1 hari bisa pake fitur
+${prefix}freeoneday <Link Gc>
+tanpa harus chat owner
+`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
